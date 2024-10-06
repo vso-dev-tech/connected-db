0ca57b8db316accd4e94012292e39274a346dcd3
Tordx <102406588+Tordx@users.noreply.github.com>
2024-10-07 03:09:02 +0800

    added api response data types

--- interface/index.ts
+++ interface/index.ts
@@ -4,8 +4,40 @@ export interface auth {
         password: string,
     }
 };
-
-export interface validation {
-    userId: string
-    headers: any
-};
+interface BodyData {
+    __collector: object;
+    blobId: string;
+    offset: number;
+    size: number;
+  }
+  
+  interface BodyBlob {
+    _data: BodyData;
+  }
+  
+  interface Headers {
+    "access-control-allow-origin": string;
+    connection: string;                    
+    "content-length": string;
+    "content-type": string; 
+    date: string;
+    etag: string;                          
+    "keep-alive": string;                  
+    "x-powered-by": string;                 
+    authorization?: string;                 
+    "content-encoding"?: string;           
+  }
+  
+  export interface ApiResponse {
+    _bodyBlob: BodyBlob;
+    _bodyInit: BodyBlob;
+    bodyUsed: boolean;
+    headers: Headers;
+    ok: boolean;
+    status: number;
+    statusText: string;
+    type: string;
+    url: string;
+    body: any;
+  }
+  
