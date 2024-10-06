16d45afed55c5053e42b38796c1b6910c7e01132
Tordx <102406588+Tordx@users.noreply.github.com>
2024-10-07 03:10:53 +0800

    added validation route

--- routes/validation/index.ts
+++ routes/validation/index.ts
@@ -1,12 +1,12 @@
 
 import express from "express";
-import {validation } from "interface";
+import { ApiResponse } from "interface/index";
 import verifyToken from "middleware/validation";
 
 const router = express.Router();
 
-router.get("/", verifyToken, (req: validation, res: any) => {
-  res.json({ message: "This is a protected route", userId: req.userId });
+router.get("/", verifyToken, (req: ApiResponse, res: any) => {
+  res.json({ message: "This is a protected route" });
 });
 
 export default router;
